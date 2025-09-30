d21c095180da6d517b26d95deb651e30de3ccfb5
Matin Parsapour <m.parsapour@dastan.co@dastan-development.dastan.co>
2025-09-30 10:49:36 -0700

    Add a relationship class for relationship between user and product

---
+++ src/com/dastan/store/entity/UserProduct.cpp
@@ -0,0 +1,9 @@
+//
+// Created by m.parsapour@dastan.co on 9/30/25.
+//
+
+#include "UserProduct.h"
+
+namespace Relationship
+{
+} // Relationship
---
+++ src/com/dastan/store/entity/UserProduct.h
@@ -0,0 +1,15 @@
+//
+// Created by m.parsapour@dastan.co on 9/30/25.
+//
+
+#ifndef STORE_USERPRODUCT_H
+#define STORE_USERPRODUCT_H
+
+namespace Relationship
+{
+    class UserProduct
+    {
+    };
+} // Relationship
+
+#endif //STORE_USERPRODUCT_H
