e94cb44981c5c59c9fda1c7ef1a280026012e06b
Matin Parsapour <m.parsapour@dastan.co@dastan-development.dastan.co>
2025-09-30 11:39:47 -0700

    Add new class to connect to database

---
+++ src/com/dastan/store/db/Connection.h
@@ -0,0 +1,15 @@
+//
+// Created by m.parsapour@dastan.co on 9/30/25.
+//
+
+#ifndef STORE_CONNECTION_H
+#define STORE_CONNECTION_H
+
+namespace Connection
+{
+    class Connection
+    {
+    };
+} // Connection
+
+#endif //STORE_CONNECTION_H
