6fb957207d721f9ccbe70eebb0661746c61038d8
Matin Parsapour <m.parsapour@dastan.co@dastan-development.dastan.co>
2025-09-30 10:01:25 -0700

    Add new class as a database, it saves tables, relations and everything

---
+++ src/com/dastan/store/db/Database.cpp
@@ -0,0 +1,8 @@
+//
+// Created by m.parsapour@dastan.co on 9/30/25.
+//
+
+#include "Database.h"
+
+namespace Schema {
+} // Schema
---
+++ src/com/dastan/store/db/Database.h
@@ -0,0 +1,13 @@
+//
+// Created by m.parsapour@dastan.co on 9/30/25.
+//
+
+#ifndef STORE_DATABASE_H
+#define STORE_DATABASE_H
+
+namespace Schema {
+    class Database {
+    };
+} // Schema
+
+#endif //STORE_DATABASE_H
